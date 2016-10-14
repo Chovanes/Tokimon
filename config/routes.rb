0e5f03c110e30f3211df88e3a70ca5d9373ab3de
Connor <chovanes@sfu.ca>
2016-10-14 02:21:26 -0700

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'tokimons#index'
+  root 'trainers#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
